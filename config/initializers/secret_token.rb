e963676ec8135341bd3dcfc092c8af812e6d9ce3
Ben Reese <reeseb@gmail.com>
2014-11-07 20:29:58 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThermoDemo::Application.config.secret_token = 'ae081e9159e69fa6f23e8ad7b2131e7ed0b9d5f0fde1c0039487bcc0258ead681de93ba7649e09c3d6c8f39e27933205f88d798d04824c8b4091159c0b522f35'
